acb1c828819a4c26ebbd23d1a81be56e795c9329
Luis Enrique Ruiz Martinez <luisenrique-rm@hotmail.com>
2025-09-04 11:56:33 -0600

    hotfix

--- format/litigacion/accionesNucsLit.php
+++ format/litigacion/accionesNucsLit.php
@@ -1390,6 +1390,9 @@ switch ($acc) {
         }
         ?>
         <tr>
+            <td class="textCent">
+                <input type="checkbox" class="tdRowMainData negr checkCarpeta" value="<?php echo $idEstatusNucsThisNUC; ?>">
+            </td>
             <td class="tdRowMainData negr"><?php echo ($sumador + 1); ?></td>
             <td class="tdRowMainData negr"><?php echo $nuc; ?></td>
             <td class="tdRowMainData negr"><?php echo $exp; ?></td>
